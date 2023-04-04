1d73dd15c1e24775785ce982bc7e5bb46c6a21ef
Yassine El Haddad <megahaddad15@gmail.com>
2023-04-04 17:17:40 +0000

    Update solong.h

--- solong.h
+++ solong.h
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 15:22:00 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/02 16:53:44 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/04 17:12:23 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,24 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-typedef struct map_line
+typedef struct map
 {
 	char			*line;
-	struct map_line	*next;
+	struct map	*next;
 }	t_list;
 
-char	*get_next_line(int fd);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strdup(char *s1);
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(char *s, int c);
-void	*ft_calloc(size_t count, size_t size);
-char	*get_next_line(int fd);
 char	*ft_substr(char *s, unsigned int start, size_t len);
 void	*ft_memcpy(void *dst, void *src, size_t n);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	*ft_calloc(size_t count, size_t size);
+int		get_next_line(int fd, char **res);
+char	*ft_strjoin(char *s1, char *s2);
 t_list	*new_map_line(char *line);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(char *s, int c);
+t_list	*getmap(char *map_path);
+char	*remove_nl(char *s1);
+char	*ft_strdup(char *s1);
 
 
 # ifndef BUFFER_SIZE
