09b5309ca0ad4d29ac622ce86c4dcfebfdc39737
Yassine El Haddad <megahaddad15@gmail.com>
2023-04-08 18:09:37 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 15:22:14 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/04 18:45:06 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/08 18:04:49 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,40 +17,68 @@ void	f(void)
 	system("leaks a.out");
 }
 
-int	all_ones(char *strig)
+int	all_ones(char *string)
 {
 	int	i;
 
-	i = -1;
-	while (strig[++ i])
+	i = 0;
+	while (string[i])
 	{
 		if (string[i] != '1')
 			return (0);
+		i ++;
 	}
 	return (1);
 }
 
 int	starts_ends_1(t_list *node)
 {
-	t_list	*node;
-	int	i;
+	int		i;
+	size_t	len;
+	t_list	*last;
 
 	i = -1;
+	last = ft_lstlast(node);
 	while (node != NULL)
 	{
+		len = ft_strlen(node->line);
 		if (node->line[0] != '1')
 			return (0);
-		if (node->line[-1] != '1')
+		else if (node->next == NULL && node->line[len - 2] != '1')
 			return (0);
-		node = node->next
+		else if (node->line[len - 1] != '1')
+			return (0);
+		node = node->next;
 	}
 	return (1);
+}
+
+int	has_invalid_char(t_list *node)
+{
+	int	i;
+	char	c;
+	char	*str;
+
+	i = 0;
+	while (node != NULL)
+	{
+		str = node->line;
+		i = 0;
+		while (str[i])
+		{
+			c = str[i];
+			if (c != 'P' && c != 'C' && c != '0' && c != '1' && c != 'E')
+				return (1);
+			i ++;
+		}
+		node = node->next;
+	}
+	return (0);
 }
 
 int is_closed(t_list *ptr)
 {
 	t_list	*map;
-	t_list	*head;
 
 	map = ptr;
 	if (!all_ones(map->line) || !all_ones(ft_lstlast(map)->line))
@@ -63,24 +91,27 @@ int is_closed(t_list *ptr)
 int	is_valid_map(t_list **ptr)
 {
 	t_list	*map;
-	t_list	*head;
 
 	map = *ptr;
-	if (ft_lstsize(*ptr) <= 3)
-		return (0);
-	if (!is_closed(&map))
-		return (0);
+	if (has_invalid_char(map))
+		return (-1);
+	if (ft_lstsize(map) <= 3)
+		return (-2);
+	if (!is_closed(map))
+		return (-3);
 	return (1);
 }
 
 int	main(int ac, char **av)
 {
 	t_list	*map;
+	(void) ac;
 
 	map = getmap(av[1]);
+	printf("%d\n", is_valid_map(&map));
 	while (map != NULL)
 	{
-		printf("%s\n", map->line);
+		printf("%s", map->line);
 		map = map->next;
 	}
 	//atexit(f);
