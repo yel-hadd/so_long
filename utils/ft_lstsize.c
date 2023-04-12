1708687217814341217a52115ef69054fb17df2d
Yassine El Haddad <yel-hadd@mail.com>
2023-04-12 01:36:00 +0000

    Update ft_lstsize.c

--- utils/ft_lstsize.c
+++ utils/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 17:23:37 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/04 17:23:50 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/12 01:34:33 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_lstsize(t_list *lst)
 {
 	int	len;
 
+	if (!lst)
+		return (0);
 	len = 0;
 	while (lst)
 	{
