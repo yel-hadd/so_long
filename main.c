1719fefab4f6bca28a219f827a995d7f13467266
Yassine El Haddad <yel-hadd@mail.com>
2023-04-12 00:45:47 +0000

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
+/*   By: yel-hadd <yel-hadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 15:22:14 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/11 00:29:47 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/11 22:48:32 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,122 +17,31 @@ void	f(void)
 	system("leaks a.out");
 }
 
-int	all_ones(char *string)
+t_player	*spawn_player(t_list *map)
 {
-	int	i;
+	t_player	*ptr;
 
-	i = 0;
-	while (string[i] && string[i] != '\n')
-	{
-		if (string[i] != '1')
-			return (0);
-		i ++;
-	}
-	return (1);
-}
+	ptr = (t_player *)malloc(sizeof(t_player));
+	if (!ptr)
+		return (ptr);
 
-int	starts_ends_1(t_list *node)
-{
-	int		i;
-	size_t	len;
-	t_list	*last;
-
-	i = -1;
-	last = ft_lstlast(node);
-	while (node != NULL)
-	{
-		len = ft_strlen(node->line);
-		if (node->line[0] != '1')
-			return (0);
-		else if (node->line[len - 1] != '1')
-			return (0);
-		node = node->next;
-	}
-	return (1);
-}
-
-int	has_invalid_char(t_list *node)
-{
-	int	i;
-	char	c;
-	char	*str;
-
-	i = 0;
-	while (node != NULL)
-	{
-		str = node->line;
-		i = 0;
-		while (str[i] != '\0')
-		{
-			c = str[i];
-			if (c != 'P' && c != 'C' && c != '0' && c != '1' && c != 'E' && c != '\n')
-				return (1);
-			i ++;
-		}
-		node = node->next;
-	}
-	return (0);
-}
-
-int is_closed(t_list *ptr)
-{
-	t_list	*map;
-
-	map = ptr;
-	if (!all_ones(map->line) || !all_ones(ft_lstlast(map)->line))
-		return (0);
-	if (!starts_ends_1(map))
-		return (0);
-	return (1);
-}
-
-int	block_count(t_list *node, char block)
-{
-	int		i;
-	char	*str;
-	int		count;
-
-	count = 0;
-	while (node != NULL)
-	{
-		i = 0;
-		str = node->line;
-		while (str[i])
-		{
-			if (str[i] == block)
-				count += 1;
-			i ++;
-		}
-		node = node->next;
-	}
-	return (count);
-}
-
-int	is_valid_map(t_list **ptr)
-{
-	t_list	*map;
-
-	map = *ptr;
-	if (ft_lstsize(map) < 3)
-		return (-2);
-	if (has_invalid_char(map))
-		return (-1);
-	if (!is_closed(map))
-		return (-3);
-	if (block_count(map, 'E') != 1 || block_count(map, 'P') != 1)
-		return (-4);
-	if (block_count(map, 'C') < 1)
-		return (-4);
-	return (1);
+	ptr->x = get_player_pos(map, 0);
+	ptr->y = get_player_pos(map, 1);
+	ptr->next = NULL;
+	return (ptr);
 }
 
 int	main(int ac, char **av)
 {
-	t_list	*map;
+	t_list		*map;
+	t_player	*p;
 	(void) ac;
 
 	map = getmap(av[1]);
-	printf("%d\n", is_valid_map(&map));
+	if (is_valid_map(&map) != 1)
+		return (0);
+	p = spawn_player(map);
+	printf("--%c\n", get_line(map, p->y)[p->x]);
 	while (map != NULL)
 	{
 		printf("%s\n", map->line);
