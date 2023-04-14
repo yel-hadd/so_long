99cf51bbef36d9e7ccc3d0ab09373ba1054590a5
Yassine El Haddad <yel-hadd@mail.com>
2023-04-14 21:35:29 +0000

    Update flood_check.c

--- utils/flood_check.c
+++ utils/flood_check.c
@@ -6,27 +6,29 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 20:50:43 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/14 20:50:55 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/14 21:28:41 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../solong.h"
 
-int	flood_check(t_list **lst)
+int flood_check(t_list **lst)
 {
-	t_list	*node;
-	int		i;
+    t_list *node;
+    int i;
+    char *str;
 
-	node = *lst;
-	while (node != NULL)
-	{
-		i = -1;
-		while(node->line[++i])
-		{
-			if (node->line[i] != '1' && node->line[i] != 'X')
-				return (ft_lstclear(lst), 0);
-		}
-		node = node->next;
-	}
-	return (ft_lstclear(lst), 1);
+    node = *lst;
+    while (node != NULL)
+    {
+        i = -1;
+        while (node->line[++i])
+        {
+            str = node->line;
+            if (str[i] != '1' && str[i] != 'X' && str[i] != '0')
+                return (ft_lstclear(lst), 0);
+        }
+        node = node->next;
+    }
+    return (ft_lstclear(lst), 1);
 }
