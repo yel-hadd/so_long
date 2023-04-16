86302f3fb2e857522cdae22e095e78f88048f695
Yassine El Haddad <yel-hadd@mail.com>
2023-04-16 23:56:13 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 15:22:14 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/16 17:27:03 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/16 23:55:25 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,35 +21,50 @@ void	draw_map(void *ml, void *win, t_map *map, t_blocks *blocks)
 {
 	char	*line;
 	int		i;
-	int		x;
+	int		y;
 
 	while (map != NULL)
 	{
 		line = map->line;
-		x = map->index;
+		y = map->index * 40;
 		i = -1;
 		while (line[++ i])
 		{
 			if (line[i] == '1')
-				mlx_put_image_to_window(ml, win, blocks->W, i * 40, x * 40);
+				mlx_put_image_to_window(ml, win, blocks->W, i * 40, y);
 			else if (line[i] == '0' || line[i] == 'E')
-				mlx_put_image_to_window(ml, win, blocks->O, i * 40, x * 40);
+				mlx_put_image_to_window(ml, win, blocks->O, i * 40, y);
 			else if (line[i] == 'C')
-				mlx_put_image_to_window(ml, win, blocks->C, i * 40, x * 40);
+				mlx_put_image_to_window(ml, win, blocks->C, i * 40, y);
 			else if (line[i] == 'P')
-				mlx_put_image_to_window(ml, win, blocks->P, i * 40, x * 40);
+				mlx_put_image_to_window(ml, win, blocks->PL, i * 40, y);
 		}
 		map = map->next;
 	}
 }
 
-void	start_game(t_map *map, t_player *p, t_blocks *b, t_mlx *m)
+/*
+W => 13
+A => 0
+S => 1
+D => 2
+ESC => 53
+*/
+
+
+int	key_hook(int keycode, t_mlx *m)
 {
-	draw_map(m->ptr, m->win, map, b);
-	mlx_loop(m->ptr);
-	free_mlx(&m);
-	ft_lstclear(&map);
-	free(p);
+	printf("%d\n", keycode);
+	printf("%s\n", m->map->line);
+	return (0);
+}
+
+void	start_game(t_mlx *game)
+{
+	draw_map(game->ptr, game->win, game->map, game->b);
+	mlx_key_hook(game->win, key_hook, game);
+	mlx_loop(game->ptr);
+	free_mlx(&game);
 }
 
 int	main(int ac, char **av)
@@ -57,8 +72,7 @@ int	main(int ac, char **av)
 	t_map		*map;
 	t_map		*copy;
 	t_player	*p;
-	t_blocks	*b;
-	t_mlx		*m;
+	t_mlx		*game;
 
 	if (ac != 2)
 		return (1);
@@ -70,8 +84,7 @@ int	main(int ac, char **av)
 	flood_fill(&copy, p->x, p->y);
 	if (flood_check(&copy) != 1)
 		return (ft_lstclear(&map), free(p), 1);
-	m = load_mlx(map, "SAAD");
-	b = load_blocks(m->ptr);
-	start_game(map, p, b, m);
+	game = load_mlx(map, p, "SAAD");
+	start_game(game);
 	//atexit(f);
 }
