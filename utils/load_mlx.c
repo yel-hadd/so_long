c53bc5a75356dd373a8e8950914f8c960c3e2729
Yassine El Haddad <yel-hadd@mail.com>
2023-04-16 23:56:21 +0000

    Update load_mlx.c

--- utils/load_mlx.c
+++ utils/load_mlx.c
@@ -6,25 +6,28 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 16:59:21 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/16 16:59:33 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/16 23:54:36 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../solong.h"
 
-t_mlx    *load_mlx(t_map *map, char *title)
+t_mlx    *load_mlx(t_map *map, t_player *p, char *title)
 {
-	t_mlx	*b;
+	t_mlx		*node;
 	int			x;
     int			y;
 
-	b = (t_mlx *)malloc(sizeof(t_mlx));
-	if (!b)
-		return (b);
+	node = (t_mlx *)malloc(sizeof(t_mlx));
+	if (!node)
+		return (node);
 	x = ft_strlen(map->line) * 40;
 	y = ft_lstsize(map) * 40;
-	b->ptr = mlx_init();
-	b->win = mlx_new_window(b->ptr, x, y, title);
-    b->next = NULL;
-	return (b);
+	node->ptr = mlx_init();
+	node->win = mlx_new_window(node->ptr, x, y, title);
+	node->b = load_blocks(node->ptr);
+	node->p = p;
+	node->map = map;
+    node->next = NULL;
+	return (node);
 }
