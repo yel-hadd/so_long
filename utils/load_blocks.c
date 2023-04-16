fd9e24176a3dd9af3c437be2aa29e1e8b81c7ee4
Yassine El Haddad <yel-hadd@mail.com>
2023-04-16 16:37:54 +0000

    Create load_blocks.c

---
+++ utils/load_blocks.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_blocks.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/15 23:54:48 by yel-hadd          #+#    #+#             */
+/*   Updated: 2023/04/16 01:41:52 by yel-hadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../solong.h"
+
+t_blocks    *load_blocks(void *mlx_ptr)
+{
+	t_blocks	*b;
+	int			var;
+    int			var1;
+
+	b = (t_blocks *)malloc(sizeof(t_blocks));
+	if (!b)
+		return (b);
+	b->C = mlx_xpm_file_to_image(mlx_ptr, "assets/C.xpm", &var, &var1);
+	b->W = mlx_xpm_file_to_image(mlx_ptr, "assets/W.xpm", &var, &var1);
+	b->E = mlx_xpm_file_to_image(mlx_ptr, "assets/E.xpm", &var, &var1);
+	b->P = mlx_xpm_file_to_image(mlx_ptr, "assets/P.xpm", &var, &var1);
+	b->O = mlx_xpm_file_to_image(mlx_ptr, "assets/O.xpm", &var, &var1);
+    b->next = NULL;
+	return (b);
+}
