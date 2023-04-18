403a46f21c3ee6335df79415f10e8bd314401ff7
Yassine El Haddad <yel-hadd@mail.com>
2023-04-18 00:45:14 +0000

    Update is_valid_map.c

--- utils/is_valid_map.c
+++ utils/is_valid_map.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 21:58:46 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/14 23:26:43 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/17 21:36:19 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int	is_valid_map(t_map **ptr)
 		return (-2);
 	if (!is_closed(map) || block_count(map, 'E') != 1)
 		return (-3);
-	if (block_count(map, 'C') < 1 || block_count(map, 'P') != 1)
+	map->coins = block_count(map, 'C');
+	if (map->coins < 1 || block_count(map, 'P') != 1)
 		return (-4);
 	if (!same_len(map))
 		return (-5);
