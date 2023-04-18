64b92ba1daa2379f240260c53290b5cdc76f963e
Yassine El Haddad <yel-hadd@mail.com>
2023-04-18 00:45:08 +0000

    Update get_line.c

--- utils/get_line.c
+++ utils/get_line.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 22:25:46 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/14 23:26:43 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/17 01:27:53 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ get map line by its index
 returns NULL if not found
 */
 
-char    *get_line(t_map *node, int index)
+char	*get_line(t_map *node, int index)
 {
 	while (node != NULL)
 	{
