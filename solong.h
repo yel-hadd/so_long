2fe22ed9144bd0d7a36b3f76e51afc784f7176a6
Yassine El Haddad <yel-hadd@mail.com>
2023-04-18 00:44:46 +0000

    Update solong.h

--- solong.h
+++ solong.h
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 15:22:00 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/16 23:54:47 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/18 00:33:16 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,12 @@
 # include <stdio.h>
 # include <mlx.h>
 
-
 typedef struct map
 {
 	char			*line;
 	int				index;
+	int				coins;
+	int				exit[2];
 	struct map		*next;
 }	t_map;
 
@@ -37,14 +38,14 @@ typedef struct player
 
 typedef struct building_blocks
 {
-	void	*PR;
-	void	*PL;
-	void	*PU;
-	void	*PD;
-	void	*C;
-	void	*E;
-	void	*O;
-	void	*W;	
+	void					*pr;
+	void					*pl;
+	void					*pu;
+	void					*pd;
+	void					*c;
+	void					*e;
+	void					*o;
+	void					*w;	
 	struct building_blocks	*next;
 }	t_blocks;
 
@@ -55,40 +56,49 @@ typedef struct mlx_pointers
 	t_map				*map;
 	t_player			*p;
 	t_blocks			*b;
+	int					exit[2];
 	struct mlx_pointers	*next;
 }	t_mlx;
 
+void		draw_map(void *ml, void *win, t_map *map, t_blocks *blocks);
 char		*ft_substr(char *s, unsigned int start, size_t len);
 t_mlx		*load_mlx(t_map *map, t_player *p, char *title);
 void		*ft_memcpy(void *dst, void *src, size_t n);
 void		ft_lstadd_back(t_map **lst, t_map *new);
 int			get_player_pos(t_map *node, int choice);
+int			block_is_coin(t_map *map, int x, int y);
+int			block_is_wall(t_map *map, int x, int y);
+int			block_is_exit(t_map *map, int x, int y);
 void		flood_fill(t_map **lst, int x, int y);
 void		*ft_calloc(size_t count, size_t size);
-int			block_count(t_map *node, char block);
 t_map		*new_map_line(char *line, int index);
-char    	*get_line(t_map *node, int index);
+int			block_count(t_map *node, char block);
+char		*get_line(t_map *node, int index);
 int			get_next_line(int fd, char **res);
 char		*ft_strtrim(char *s1, char *set);
 char		*ft_strjoin(char *s1, char *s2);
+t_blocks	*load_blocks(void *mlx_ptr);
 int			has_invalid_char(t_map *node);
-t_blocks    *load_blocks(void *mlx_ptr);
+t_player	*spawn_player(t_map *map);
 int			starts_ends_1(t_map *node);
 char		*ft_strchr(char *s, int c);
 int			is_valid_map(t_map **ptr);
-t_player	*spawn_player(t_map *map);
 void		ft_lstdelone(t_map *lst);
 void		ft_lstclear(t_map **lst);
 int			flood_check(t_map **lst);
 t_map		*ft_lstlast(t_map *lst);
 t_map		*ft_lstcopy(t_map *lst);
 t_map		*getmap(char *map_path);
+void		move_right(t_mlx *game);
 int			ft_lstsize(t_map *lst);
 int			all_ones(char *string);
+void		move_down(t_mlx *game);
+void		move_left(t_mlx *game);
 int			is_closed(t_map *ptr);
 int			same_len(t_map *node);
 void		free_mlx(t_mlx **ptr);
 char		*ft_strdup(char *s1);
+void		move_up(t_mlx *game);
 int			ft_strlen(char *s);
 
 # ifndef BUFFER_SIZE
